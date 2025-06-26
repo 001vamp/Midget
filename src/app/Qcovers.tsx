4f3d88ca6df66d819a379a0d15cfd73fba64720e
001vamp <dirtymycup@gmail.com>
2025-06-26 16:15:07 -0400

    adjusted layout and drop shadow

--- src/app/Qcovers.tsx
+++ src/app/Qcovers.tsx
@@ -4,12 +4,12 @@ export default function Qcovers() {
         <div>
 
 
-            <div id="AlbumCover" className=" bg-[url('https://media.pitchfork.com/photos/682ca1b181b971ad1da2139a/master/w_1280%2Cc_limit/skaiwater-pinkPrint.jpeg')] bg-contain bg-center w-28 h-28
+            <div id="AlbumCover" className=" bg-[url('https://media.pitchfork.com/photos/682ca1b181b971ad1da2139a/master/w_1280%2Cc_limit/skaiwater-pinkPrint.jpeg')] bg-contain bg-center w-30 h-30
           rounded-md mt-4 absolute z-10 top-0 left-4 shadow-[1px_0px_4px_2px] shadow-blue-500/50"></div>
 
-            <div id="AlbumCover" className=" bg-[url('https://media.pitchfork.com/photos/6453ab5e575213cbefca6d9a/master/w_1280%2Cc_limit/Destroy-Lonely-If-Looks-Could-Kill.jpg')] bg-contain bg-center w-26 h-26 shadow-[10px_7px_10px_rgba(3,5,30,0.5)] rounded-md mt-2 absolute z-8 top-8 bottom-0 right-0 left-12"></div>
+            <div id="AlbumCover" className=" bg-[url('https://media.pitchfork.com/photos/6453ab5e575213cbefca6d9a/master/w_1280%2Cc_limit/Destroy-Lonely-If-Looks-Could-Kill.jpg')] bg-contain bg-center w-28 h-28 shadow-[10px_7px_10px_rgba(3,5,30,0.35)] rounded-md mt-2 absolute z-8 top-8 bottom-0 right-0 left-11"></div>
 
-            <div id="AlbumCover" className=" bg-[url('https://i.scdn.co/image/ab67616d0000b273f7c26ba8a592c4921cb083b3')] bg-contain bg-center w-24 h-24 rounded-md mt-2 absolute z-4 top-14 bottom-0 right-2 left-20  shadow-[15px_12px_10px_rgba(3,5,30,0.5)]"></div>
+            <div id="AlbumCover" className=" bg-[url('https://i.scdn.co/image/ab67616d0000b273f7c26ba8a592c4921cb083b3')] bg-contain bg-center w-26 h-26 rounded-md mt-2 absolute z-4 top-14 bottom-0 right-2 left-18  shadow-[15px_12px_10px_rgba(3,5,30,0.35)]"></div>
 
 
         </div>
